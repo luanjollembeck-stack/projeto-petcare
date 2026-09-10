7a09667e2f2cbe1e25efa4078df9315904051473
luanjollembeck-stack <luan_jollembeck@estudante.sesisenai.org.br>
2026-09-10 19:20:46 -0300

    feat

--- backend/backend/src/server.ts
+++ backend/backend/src/server.ts
@@ -1,20 +1,15 @@
 import express, { type Request, type Response } from "express"
 import { randomUUID } from "node:crypto"
 import { pool } from "./database/connection.js"
-
 const app = express()
 const port = 3000
-
 app.use(express.json())
-
 app.use("/cliente")
-
 app.get("/health", (_request: Request, response: Response) => {
     return response.json({
         status: "ok"
     })
 })
-
 app.get("/", async (_request: Request, response: Response) => {
     try{
         const res = await pool.query("SELECT * FROM clientes")
@@ -24,11 +19,9 @@ app.get("/", async (_request: Request, response: Response) => {
         console.error(error);
     }
 })
-
 interface CreateUserBody {
     name: string;
 }
-
 app.post("/users", (request: Request<object, object,
     CreateUserBody>, Response: Response) => {
     const name = request.body.name?.trim()
@@ -37,16 +30,11 @@ app.post("/users", (request: Request<object, object,
                 error: "Name is required"
             })
         }
-
         return Response.status(201).json({
             id: randomUUID(),
             name
         })
 })
-
-
-
-
 app.listen(port,()=> {
     console.log(`API rodando em http://localhost:${port}`);
 })
