e14f62262aa8340aac1988a009b23a2ffe3d21c7
luanjollembeck-stack <luan_jollembeck@estudante.sesisenai.org.br>
2026-09-14 19:23:46 -0300

    fix
    conflito

--- backend/backend/src/routes/cliente.route.ts
+++ backend/backend/src/routes/cliente.route.ts
@@ -1,14 +1,32 @@
-import express, { Router, type Request, type Response } from "express"
+import { Router, type Request, type Response } from "express"
 import { clienteService } from "../services/cliente.service.js"
+import { CriarCliente } from "../types/types.js"
 
-export const clienteRouter = Router() 
+export const clienteRouter = Router()
 
-clienteRouter.get("/cliente", async (_request: Request, response: Response) => {
-    try{
+clienteRouter.get("/", async (_request: Request, response: Response) => {
+    try {
         const res = await clienteService.getAll()
-        
-        response.json(res)
-    }catch (error) {
+        return response.json(res)
+    } catch (error) {
         console.error(error);
+        return response.status(500).json({
+            error: "Erro Interno"
+        })
+    }
+})
+
+clienteRouter.post("/", async (request: Request<{}, {}, CriarCliente>, response: Response) => {
+    try {
+        const dados = request.body
+
+        const cliente = await clienteService.create(dados)
+        return response.status(201).json(cliente)
+    } catch (error) {
+        console.error(error);
+
+        return response.status(500).json({
+            error: "Erro Interno"
+        })
     }
 })
--- backend/backend/src/services/cliente.service.ts
+++ backend/backend/src/services/cliente.service.ts
@@ -1,14 +1,23 @@
-import { pool } from "../database/connection.js"
-import { Cliente, CriarCliente } from "../types/cliente.js"
+import { pool } from "../database/connection.js";
+import { Cliente, CriarCliente } from "../types/types.js";
 
 class ClienteService {
     async getAll(): Promise<Cliente[]> {
-        const res = await pool.query("SELECT * FROM clientes")
-
+        const res = await pool.query<Cliente>("SELECT * FROM clientes")
         return res.rows
-    } catch (error) {
-        console.error(error);
+    }
+
+    async create(dados: CriarCliente): Promise<Cliente> {
+        const res = await pool.query<Cliente>(`INSERT INTO clientes (nome, telefone, idade, email) VALUES ($1, $2, $3, $4) RETURNING *`, [dados.nome, dados.telefone, dados.idade, dados.email])
+
+        const cliente = res.rows[0]
+
+        if (!cliente) {
+            throw new Error("O banco não retornou o cliente cadastrado");
+        }
+
+        return cliente
+
     }
 }
-
 export const clienteService = new ClienteService()
